4392486c7111b21c6fb641d5d53d10ed97aa3794
Marcus Will <marcusxwill@gmail.com>
2024-02-22 18:14:05 +0100

    fix hot reload

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone'
+  output: 'standalone',
+  webpack: (config, _) => ({
+    ...config,
+    watchOptions: {
+      ...config.watchOptions,
+      poll: 800,
+      aggregateTimeout: 300,
+    },
+  }),
 };
 
 export default nextConfig;
