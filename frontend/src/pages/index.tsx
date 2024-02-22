1c7009124bbef8c587d7750771187be98667a24e
Marcus Will <marcusxwill@gmail.com>
2024-02-22 15:23:59 +0100

    docker config inter

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google';
+import Image from 'next/image';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -42,6 +42,8 @@ export default function Home() {
           height={37}
           priority
         />
+        <div>LOGO</div>
+        asdasd
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
